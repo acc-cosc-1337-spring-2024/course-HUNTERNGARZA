879ea95c217d2625267c28a6bd8688afbf78cf69
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-01-26 02:47:48 +0000

    Homework 1

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,4 @@
-#
+import output
+
+result: int = output.multiply_numbers(5, 5)
+print(result)
--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,6 @@
 def get_number(num):
     return num
+
+def multiply_numbers(a: int, b: int) -> int:
+    result: int = a * b
+    return result
