67a48f5f97692d65acbcdcc22af4a9d1d6f771b2
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-02-14 03:17:57 +0000

    Completed HW 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,28 @@
-#
+import repetition
+
+
+while True:
+    user_input: str = input("""Homework 3 Menu
+1-Factorial
+2-Sum odd numbers
+3-Exit
+    """)
+
+    if user_input == "1":
+        user_number = 0
+
+        while not (user_number > 0 and user_number < 10):
+            user_number = int(input())
+            repetition.get_factorial(user_number)
+
+    elif user_input == "2":
+        user_number = 0
+
+        while not  (user_number > 0 and user_number < 100):
+            user_number = int(input())
+            repetition.sum_odd_numbers(user_number)
+
+    elif user_input == "3":
+        if input("continue? ") == "no":
+            break
+
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,14 @@
-#
+def get_factorial(num: int) -> int:
+
+    total = 1
+    i: int
+    for i in range(1, num + 1):
+        total *= i
+    
+    return total
+
+
+def sum_odd_numbers(num: int) -> int:
+    return sum(
+        [i for i in range(0, num + 1) if (i%2 != 0)]
+    )
