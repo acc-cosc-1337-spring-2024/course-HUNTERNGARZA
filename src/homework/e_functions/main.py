2ac9916c746f5221288d11315fb9a6bc85e91f5d
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-02-23 18:15:38 +0000

    Homework 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,8 @@
-#
+import value_return
+
+
+epoch_seconds = 3800
+
+print(value_return.get_hour(epoch_seconds))
+print(value_return.get_minutes(epoch_seconds))
+print(value_return.get_seconds(epoch_seconds))
