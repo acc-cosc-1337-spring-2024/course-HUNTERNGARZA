2e7c474b07f2da2291c4b9354dd1316b98bfddba
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-02-28 03:10:32 +0000

    Homework 5

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,24 @@
-#
+import strings
+
+while True:
+    user_input: str = input("""1-Hamming Distance
+2-Dna Complement
+3-Exit
+""")
+
+    user_input = int(user_input)
+
+    if user_input == 1:
+        s = input("dna1: ")
+        s2 = input("dna2: ")
+        out = strings.get_hamming_distance(s, s2)
+        print(out)
+    
+    elif user_input == 2:
+        s = input("dna: ")
+        out = strings.get_dna_complement(s)
+        print(out)
+    
+    else:
+        break
+
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,28 @@
-#
+def get_hamming_distance(dna1: str, dna2: str):
+    hamming_distance = 0
+
+    for i in range(len(dna1)):
+        dna1_symbol: str = dna1[i]
+        dna2_symbol: str = dna2[i]
+
+        if dna1_symbol != dna2_symbol:
+            hamming_distance += 1
+    
+    return hamming_distance
+
+
+def get_dna_complement(dna: str) -> str:
+    reversed_dna = "".join(reversed(dna))
+
+    def f(x):
+        if x == "A":
+            return "T"
+        elif x == "T":
+            return "A"
+        elif x == "C":
+            return "G"
+        return "C"
+
+    reversed_complement: map[str] = map(f, reversed_dna)
+
+    return "".join(reversed_complement)
